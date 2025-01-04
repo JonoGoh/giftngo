5030a4673eb4d4d8d0d1827f6e71018481e3bb1d
jonogoh <gohchanj@hotmail.co.uk>
2025-01-04 16:24:03 +0000

    Initial Commit

---
+++ src/main/java/dev/jonogoh/giftngo/GiftngoApplication.java
@@ -0,0 +1,13 @@
+package dev.jonogoh.giftngo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftngoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftngoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/jonogoh/giftngo/GiftngoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jonogoh.giftngo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftngoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
